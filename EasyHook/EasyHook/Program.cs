58f064820c655321ce70726000ca0110f6c34ce1
AnyangYu <251581745@qqq.com>
2018-10-18 13:52:41 +0800

    create a VS solution; add easyhook reference; add local hook sample;

---
+++ EasyHook/EasyHook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyHook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var localHookSample = new LocalHookSample();
+            localHookSample.Run();
+        }
+    }
+}
